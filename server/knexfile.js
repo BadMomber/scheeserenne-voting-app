54f2bcbb4442ca8b6d255313ad8d9bc5ffb5c7db
Kersten Kriegbaum <kersten@badmomber.com>
2020-03-06 19:53:13 +0100

    fixed connection to db in production

--- server/knexfile.js
+++ server/knexfile.js
@@ -31,12 +31,7 @@ module.exports = {
 
   production: {
     client: "postgresql",
-    connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,

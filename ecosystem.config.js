0b792906f95e55d07f2d39cc75d38136c289722a
Kersten Kriegbaum <kersten@badmomber.com>
2022-06-01 13:05:32 +0200

    new ecosystem file for linux server deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,6 +11,10 @@ module.exports = {
       max_memory_restart: "512M",
       env: {
         NODE_ENV: "development",
+        REDIS_URL: "redis://localhost:6379/",
+        DATABASE_URL: "postgres://kk@localhost:5432/sva",
+        HOST: "127.0.0.1",
+        PORT: 4000,
       },
       env_production: {
         NODE_ENV: "production",
@@ -32,6 +36,8 @@ module.exports = {
       max_memory_restart: "512M",
       env: {
         NODE_ENV: "development",
+        HOST: "127.0.0.1",
+        PORT: 3000,
       },
       env_production: {
         NODE_ENV: "production",
@@ -43,11 +49,11 @@ module.exports = {
 
   deploy: {
     production: {
-      user: "kersten",
+      user: "kk",
       host: "192.168.178.65",
       ref: "origin/master",
       repo: "git@github.com:BadMomber/scheeserenne-voting-app.git",
-      path: "/home/kersten/production",
+      path: "/home/kk/production",
       "post-deploy":
         "npm --prefix server install && npm --prefix client install && npm --prefix server run migrate && npm --prefix client run build && pm2 reload ecosystem.config.js --env production",
       env: {

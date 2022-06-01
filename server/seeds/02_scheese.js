14934625e66ac11731c024d65453dd4c0c099d40
Kersten Kriegbaum <kersten@badmomber.com>
2022-06-01 14:08:47 +0200

    new ecosystem file for linux server deploy

--- server/seeds/02_scheese.js
+++ server/seeds/02_scheese.js
@@ -1,12 +1,28 @@
 const faker = require("faker");
 const { range } = require("lodash");
 
-exports.seed = knex => {
+const pgsql = require("knex")({
+  client: "postgresql",
+  connection: process.env.DATABASE_URL,
+  useNullAsDefault: true,
+});
+
+pgsql
+  .raw("SELECT 1")
+  .then(() => {
+    console.log("PostgreSQL connected");
+  })
+  .catch((e) => {
+    console.log("PostgreSQL not connected");
+    console.error(e);
+  });
+
+exports.seed = (knex) => {
   function getRandomInt(max) {
     return Math.floor(Math.random() * max) + 1;
   }
 
-  const scheese = range(10).map(index => ({
+  const scheese = range(10).map((index) => ({
     name: faker.internet.userName().toLowerCase(),
     image: "https://badmomber.com/scheese/00" + getRandomInt(4) + ".jpeg",
   }));

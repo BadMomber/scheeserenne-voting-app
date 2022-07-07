113a262ab5675f188d7b4d3e479f79a1d4b23a7b
Kersten Kriegbaum <kersten@badmomber.com>
2022-07-07 23:58:57 +0200

    correct results

--- client/components/Navigation.vue
+++ client/components/Navigation.vue
@@ -1,9 +1,9 @@
 <template class="navigation">
   <div>
     <b-nav tabs fill>
-      <b-nav-item id="index" href="/">Kreative Scheese</b-nav-item>
-      <b-nav-item id="voting2"  href="/voting2">Kinderscheese</b-nav-item>
-      <b-nav-item id="voting3" href="/voting3">Schnelle Scheese</b-nav-item>
+      <b-nav-item id="index" href="/">Erwachsene</b-nav-item>
+      <b-nav-item id="voting2"  href="/voting2">Bis 10 Jahre</b-nav-item>
+      <b-nav-item id="voting3" href="/voting3">Bis 14 Jahre</b-nav-item>
     </b-nav>
   </div>
 </template>
